aaeb1365b380941f94d77db8ecc3cf1bec132ecd
TiganyZ <tiganyzarrouk@gmail.com>
2021-09-04 17:22:27 +0100

    Added to ti fitting. And sorted out some formatting. finish table

---
+++ kcl_thesis/titanium/auto/ti_fitting.el
@@ -0,0 +1,45 @@
+(TeX-add-style-hook
+ "ti_fitting"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("url" "hyphens") ("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "url"
+    "float"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org41025e0"
+    "ch:ti_fitting"
+    "sec:orge068ceb"
+    "sec:org711ffe9"
+    "fig:d_sd_bond_integrals"
+    "eq:loss_function"
+    "table:titanium_objective_function"
+    "sec:orgc242e48"))
+ :latex)
+
